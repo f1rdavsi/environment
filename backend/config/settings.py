08afb46d54f4c16acbd4df665a4bfacd64068b0d
Muhsin Behbudov <127545106+CodeByMaks@users.noreply.github.com>
2025-10-01 21:16:43 +0500

    Update settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -81,11 +81,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'environment_db',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': 'mysql',
-        'PORT': '3306',
+        'NAME': os.environ.get('DB_NAME', 'environment_db'),
+        'USER': os.environ.get('DB_USER', 'root'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', 'root'),
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'PORT': os.environ.get('DB_PORT', '3306'),
     }
 }
 
